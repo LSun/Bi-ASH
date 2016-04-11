933aa8ca0779e290fa554b8b51344130e3677541
LSun <lsunchina@gmail.com>
2016-04-11 13:51:43 -0500

    comments

--- code/R/bi-ash.R
+++ code/R/bi-ash.R
@@ -1,3 +1,34 @@
+# This file contains methods for optimizing the mixture proportions given data
+# from known mixture components.
+# The penalized likelihood being maximized
+# is \sum_j \log \sum_k\sum_l \pi_k\pi_l f_{jkl} + \sum_j (prior_j-1) \log \pi_k + 
+# Adapted from Matthew Stephens's ashr package.
+
+
+
+#' @title Estimate mixture proportions of a mixture model by EM algorithm
+#'
+#' @description Given the individual component likelihoods for a 3-way mixture model, estimates the mixture proportions by an EM algorithm.
+#'
+#' @details Fits a 3-way (k,l) component mixture model \deqn{f(x|\pi)= \sum_k\sum_l \pi_{k}\pi_l f_{kl}(x)} to independent
+#' and identically distributed data \eqn{x_1,\dots,x_n}. 
+#' Estimates mixture proportions \eqn{\pi_k} and \eqn{\pi_l} by maximum likelihood, or by maximum a posteriori (MAP) estimation for a Dirichlet prior on \eqn{\pi} 
+#' (if a prior is specified).  Uses the SQUAREM package to accelerate convergence of EM. Used by the bimixEM main function; there is no need for a user to call this 
+#' function separately, but it is exported for convenience.
+#'
+#' 
+#' @param array_lik, a 3-way n by k by L matrix with (j,k,l)th element equal to \eqn{f_{kl}(x_j)}.
+#' @param prior, a k+l vector of the parameters of the Dirichlet prior on \eqn{\pi_k} (first k) and \eqn{\pi_l} (last l). Recommended to be rep(1,k+l)
+#' @param pi_init, the initial value of \eqn{\pi_k} and \eqn{\pi_l} to use. If not specified defaults to (1/k,...,1/k,1/l,...,1/l).
+#' @param control A list of control parameters for the SQUAREM algorithm, default value is set to be control.default=list(K = 1, method=3, square=TRUE, step.min0=1, step.max0=1, mstep=4, kr=1, objfn.inc=1,tol=1.e-07, maxiter=5000, trace=FALSE). 
+#' 
+#' @return A list, including the estimates (pihat)=c(\eqn{\pi_k},\eqn{\pi_l}), the log likelihood for each interation (B)
+#' and a flag to indicate convergence
+#'  
+#' @export
+#' 
+#' 
+
 bimixEM = function (array_lik, prior, pi_init = NULL, control = list()) 
 {
   control.default = list(K = 1, method = 3, square = TRUE, 
@@ -17,6 +48,8 @@ bimixEM = function (array_lik, prior, pi_init = NULL, control = list())
   return(list(pihat = 2* normalize(pmax(0, res$par)), B = res$value.objfn, 
               niter = res$iter, converged = res$convergence))
 }
+
+# helper functions used by mixEM
 
 bifixpoint = function(pi, array_lik, prior){  
   K = dim(array_lik)[1]
--- code/R/bilik.R
+++ code/R/bilik.R
@@ -1,3 +1,13 @@
+#' @title bilik
+#' @description compute the density of the components of the normal mixture m when, in group0, convoluted with a normal with standard deviation s, whereas in group1, convoluted with another normal mixture and a normal with standard deviation s, the density is evaluated at x
+#' @param betahat0 the observed \hat\beta in group0
+#' @param sebetahat0 the observed standard deviation \hat\s in group0
+#' @param betahat1 the observed \hat\beta in group1
+#' @param sebetahat1 the observed standard deviation \hat\s in group1
+#' @param sigmaalpha a grid of \sigma for the mixture model of the effect in both groups
+#' @param sigmagamma a grid of \sigma for the mixture model of theeffect only in group1
+#' @return a n by k by l array of densities
+
 bilik = function (betahat0, sebetahat0, betahat1, sebetahat1, sigmaalpha, sigmagamma) {
   N1 = length(betahat0)
   N2 = length(betahat1)
