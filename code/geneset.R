c401780734694b86b43e3f902e52bde1226ca71b
Lei Sun <sunl@uchicago.edu>
2019-08-17 08:27:40 -0500

    p53

--- code/geneset.R
+++ code/geneset.R
@@ -10,8 +10,10 @@ z <- -sign(t) * qnorm(p/2)
 X <- lim$coefficients[, 2]
 s <- X / z
 
+p53.dat <- p53[-which(abs(X / s) >= 4), ]
+
 gene.num <- length(X)
-gene.names <- rownames(p53)
+gene.names <- rownames(p53.dat)
 gene.set.all <- msigdb::read.gmt("~/GitHub/Bi-ASH/data/c2.symbols.gmt")
 gene.set.num <- length(gene.set.all$genesets)
 gene.set.size.original <- sapply(gene.set.all$genesets, length)
