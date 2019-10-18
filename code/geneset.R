35ee2b961572a85645cca7ff43800417c32ac632
LSun <sunl@uchicago.edu>
2019-10-18 17:25:35 -0500

    plot

--- code/geneset.R
+++ code/geneset.R
@@ -20,8 +20,9 @@ z <- -sign(t) * qnorm(p/2)
 X <- lim$coefficients[, 2]
 s <- X / z
 
-gene.num <- length(X)
+gene.num <- length(z)
 gene.names <- rownames(p53)
+
 gene.set.all <- msigdb::read.gmt("~/GitHub/Bi-ASH/data/c2.symbols.gmt")
 gene.set.num <- length(gene.set.all$genesets)
 gene.set.size.original <- sapply(gene.set.all$genesets, length)
@@ -62,46 +63,106 @@ res.list <- readRDS("~/GitHub/Bi-ASH/output/p53.res.rds")
 res <- res.list[[1]]
 gene.set.order <- res.list[[2]]
 
-pdf("~/GitHub/Bi-ASH/output/genesetsize.pdf", width = 6, height = 4)
-hist(gene.set.size, breaks = 50, xlab = "number of genes in a gene set", main = "Histogram of gene set sizes")
+p53 <- readRDS("~/GitHub/Bi-ASH/data/p53.rds")
+design <- model.matrix(~colnames(p53))
+lim <- limma::lmFit(p53, design)
+r.ebayes <- limma::eBayes(lim)
+p <- r.ebayes$p.value[, 2]
+t <- r.ebayes$t[, 2]
+z <- -sign(t) * qnorm(p/2)
+X <- lim$coefficients[, 2]
+s <- X / z
+which(abs(X / s) >= 4)
+
+p53 <- p53[-which(abs(X / s) >= 4), ]
+design <- model.matrix(~colnames(p53))
+lim <- limma::lmFit(p53, design)
+r.ebayes <- limma::eBayes(lim)
+p <- r.ebayes$p.value[, 2]
+t <- r.ebayes$t[, 2]
+z <- -sign(t) * qnorm(p/2)
+X <- lim$coefficients[, 2]
+s <- X / z
+
+z.model <- z
+
+p53 <- readRDS("~/GitHub/Bi-ASH/data/p53.rds")
+design <- model.matrix(~colnames(p53))
+lim <- limma::lmFit(p53, design)
+r.ebayes <- limma::eBayes(lim)
+p <- r.ebayes$p.value[, 2]
+t <- r.ebayes$t[, 2]
+z <- -sign(t) * qnorm(p/2)
+X <- lim$coefficients[, 2]
+s <- X / z
+
+z.plot <- z
+z.plot[abs(z) < 4] <- z.model
+
+gene.num <- length(z.plot)
+gene.names <- names(z.plot)
+
+pdf("~/GitHub/Bi-ASH/output/genesetintro.pdf", width = 10, height = 5)
+par(mfrow = c(1, 2))
+hist(z.plot, prob = TRUE, xlab = "z-score", main = expression("Histogram of z-scores of all genes"))
+lines(seq(-4, 4, by = 0.1), dnorm(seq(-4, 4, by = 0.1)), col = "blue", lty= 2, lwd = 2)
+legend("topright", "N(0,1)", lty = 2, col = "blue", lwd = 2)
+hist(gene.set.size, breaks = 50, xlab = "number of genes in a gene set", main = expression(paste("Histogram of gene set sizes ", p[1])))
 dev.off()
 
-topnumber <- 6
-res.diff <- c(4, 15, 374, 332)
-res.diff.original <- gene.set.order[res.diff]
-geneset.col <- scales::hue_pal()(3)
-geneset.col <- rep(geneset.col, c(1, 1, 2))
-all.geneset.col <- rep(1, gene.set.num)
-all.geneset.col[1 : topnumber] <- "red"
-all.geneset.col[res.diff] <- geneset.col
-all.geneset.pch <- rep(1, gene.set.num)
-all.geneset.pch[1 : topnumber] <- 10
-all.geneset.pch[res.diff[-1]] <- 19
+topnumber <- 10
+
 pdf("~/GitHub/Bi-ASH/output/genesetranking.pdf", width = 5, height = 5)
 par(mar=c(5.1, 4.1, 2.1, 2.1))
 plot(res$avgloglikratio, ylab = "enrichment score", cex = 1.5,
-     pch = all.geneset.pch,
-     col = all.geneset.col)
-text(setdiff(seq(topnumber), res.diff[1]), res$avgloglikratio[setdiff(seq(topnumber), res.diff[1])], res$geneset[setdiff(seq(topnumber), res.diff[1])], pos = 4, cex = 0.75, col = "red")
-text(res.diff[1], res$avgloglikratio[res.diff[1]], res$geneset[res.diff[1]], adj = c(-0.075, 1), cex = 0.75, col = geneset.col[1])
-text(res.diff[2], res$avgloglikratio[res.diff[2]], res$geneset[res.diff[2]], pos = 4, cex = 0.75, col = geneset.col[2])
-text(res.diff[3], res$avgloglikratio[res.diff[3]], res$geneset[res.diff[3]], adj = c(0.25, -1.2), cex = 0.75, col = geneset.col[3])
-text(res.diff[4], res$avgloglikratio[res.diff[4]], res$geneset[res.diff[4]], adj = c(0.85, -1.2), cex = 0.75, col = geneset.col[4])
+     pch = c(rep(10, topnumber), rep(1, 522 - topnumber)),
+     col = c(rep("red", topnumber), rep(1, 522 - topnumber)))
 dev.off()
 
-pdf("~/GitHub/Bi-ASH/output/genesetz.pdf", width = 9, height = 6)
-hist(z, prob = TRUE,
-     ylim = c(-0.02 * length(res.diff), dnorm(0)),
-     xlab = "z-score", main = "Histogram of all z-scores")
-legend("topleft", col = "blue", lty = 2, "N(0,1)", bty = "n", cex = 1)
-lines(seq(-6, 6, by = 0.1), dnorm(seq(-6, 6, by = 0.1)), lty = 2, col = "blue")
-for (i in seq(res.diff.original)) {
-  gene.set <- gene.set.all$genesets[[res.diff.original[i]]]
+pdf("~/GitHub/Bi-ASH/output/genesetz.pdf", width = 10, height = 12)
+
+par(mar=c(5.1, 4.1, 1.1, 2.1))
+
+hist(z.plot, prob = TRUE,
+     ylim = c(-0.20 * (topnumber + 2.5), 0.26),
+     main = "",
+     yaxt = 'n',
+     xaxt = "n",
+     ylab = "",
+     xlab = "z-score",
+     xlim = c(-5.5, 5.5))
+axis(side = 1, at = seq(-6, 6, by = 1))
+lines(seq(-4, 4, by = 0.1), dnorm(seq(-4, 4, by = 0.1)), col = "blue", lty= 2, lwd = 2)
+legend("topright", "N(0,1)", lty = 2, col = "blue", lwd = 2)
+
+gene.set.chosen.order <- gene.set.order[1 : topnumber]
+gene.set.chosen.common <- gene.set.order[c(1, 2, 4, 5, 6, 10)]
+gene.set.chosen.have <- gene.set.order[c(3, 7, 8, 9)]
+gene.set.chosen.havenot <- gene.set.order[c(326, 369)]
+geneset.col <- scales::hue_pal()(3)
+
+for (i in seq(gene.set.chosen.common)) {
+  gene.set <- gene.set.all$genesets[[gene.set.chosen.common[i]]]
   gene.set.index <- purrr::discard(match(gene.set, gene.names), is.na)
-  x1 <- X[gene.set.index]
-  s1 <- s[gene.set.index]
-  z1 <- x1 / s1
-  points(z1, rep(-0.02 * i, length(gene.set.index)), col = geneset.col[i], pch = i + 1)
-  text(-4.3, -0.02 * i, pos = 4, gene.set.name[res.diff.original[i]], cex = 0.75, col = geneset.col[i])
+  z1 <- z.plot[gene.set.index]
+  points(z1, rep(-0.2 * i, length(gene.set.index)), col = geneset.col[1], pch = i + 1)
+  text(-6, -0.2 * i, pos = 4, gene.set.name[gene.set.chosen.common[i]], cex = 1, col = geneset.col[1])
 }
+
+for (i in seq(gene.set.chosen.have)) {
+  gene.set <- gene.set.all$genesets[[gene.set.chosen.have[i]]]
+  gene.set.index <- purrr::discard(match(gene.set, gene.names), is.na)
+  z1 <- z.plot[gene.set.index]
+  points(z1, rep(-0.2 * (i + length(gene.set.chosen.common)), length(gene.set.index)), col = geneset.col[2], pch = i + 1 + length(gene.set.chosen.common))
+  text(-6, -0.2 * (i + length(gene.set.chosen.common)), pos = 4, gene.set.name[gene.set.chosen.have[i]], cex = 1, col = geneset.col[2])
+}
+
+for (i in seq(gene.set.chosen.havenot)) {
+  gene.set <- gene.set.all$genesets[[gene.set.chosen.havenot[i]]]
+  gene.set.index <- purrr::discard(match(gene.set, gene.names), is.na)
+  z1 <- z.plot[gene.set.index]
+  points(z1, rep(-0.2 * (i + length(gene.set.chosen.common) + length(gene.set.chosen.have)), length(gene.set.index)), col = geneset.col[3], pch = i + 1 + length(gene.set.chosen.common + length(gene.set.chosen.have)))
+  text(-6, -0.2 * (i + length(gene.set.chosen.common) + length(gene.set.chosen.have)), pos = 4, gene.set.name[gene.set.chosen.havenot[i]], cex = 1, col = geneset.col[3])
+}
+
 dev.off()
